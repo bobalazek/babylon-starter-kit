55944fab0faf81d1be5b75034696f82d1ff8b25d
Borut Balazek <bobalazek124@gmail.com>
2018-01-21 19:57:34 +0100

    Create CharacterHelpers.ts

---
+++ src/Framework/Gameplay/Character/CharacterHelpers.ts
@@ -0,0 +1,20 @@
+/********** Enums **********/
+
+export enum CharacterStatusEnum {
+    Idle,
+    Walking,
+    Running,
+    Crouching,
+    Swimming,
+    Jumping,
+    Falling,
+    Standing,
+    Aiming,
+    Throwing,
+    Seated,
+    Crouched,
+    Grounded,
+    Drowned,
+    Airborne,
+    Ragdoll
+}
